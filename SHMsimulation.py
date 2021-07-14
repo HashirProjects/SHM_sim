0dfa5c26b722dc317a956c2359507c39d813a635
HashirProjects <hashirrana2001@gmail.com>
2021-07-14 19:27:44 +0100

    added non jitted version of findCritDamp

--- SHMsimulation.py
+++ SHMsimulation.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import time
+import numba
 
 class sim():
-	def __init__(self,mass,springConstant,displacementOfSpring,initialVelocity = 0):
+	def __init__(self,mass,springConstant,displacementOfSpring,damping = 0.99,initialVelocity = 0):
 		self.mass=mass #g - 9.81
 		self.springConstant=springConstant
 		self.displacement=displacementOfSpring
@@ -12,6 +13,7 @@ class sim():
 		self.ke = (1/2)*(initialVelocity^2)*self.mass
 		self.time=0
 		self.changeInHeight=0
+		self.damping = damping
 
 	def simulate(self,time = 0.0001):#simulate the velocity of the particle after small amount of time specifed, retains the conditions from the prior simulation
 
@@ -20,32 +22,41 @@ class sim():
 		self.displacement += self.v*time + (1/2)*a*time*time
 		self.v += a*time
 		self.time+= time
+		self.v = self.v*self.damping
 
 def timefunc(func):
-	def wrapper(*arg):
+	def wrapper(*arg, **kwargs):
 		startTime = time.time()
-		results = func(*arg)
+		results = func(*arg, **kwargs)
 		endTime = time.time()
 		print(f"{func} took {endTime-startTime}s to complete.")
 		return results
 	return wrapper
 
-def main(*arg):
+def main(*arg, **kwargs):
 
-	dataV=[]
-	dataTime=[]
-	system=sim(arg[0],arg[1],arg[2])
+	system=sim(*arg, **kwargs)
 
-	for i in range (1000000):
+	while True:
 		system.simulate()
-		dataV.append(system.v)
-		dataTime.append(system.time)
+		if abs(system.v) < 0.0001:
+			break
+
+	print(system.time)
+	return system.time
+		
+
+def findCritDamp():
+
+	dampings = np.arange(1,0.9999, -0.000001)
+	print(dampings)
+	times = []
 
 
-	return (dataTime, dataV)
+	for damp in dampings:
+		times.append(main(1,1,100, damping = damp))
 
+	plt.plot(dampings, times)
+	plt.show()
 
-dataTime, dataV =timefunc(main)(1,1,10)
-plt.plot(dataTime, dataV)
-plt.show()
-
+timefunc(findCritDamp)()
